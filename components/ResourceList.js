af99a90866faaf7d20780e0a6662e3d422753fb0
Vehikl Team <go@vehikl.com>
2020-07-13 17:56:03 -0400

    Modify graphql to fetch first 50 products, change render code to use correct data returned

--- components/ResourceList.js
+++ components/ResourceList.js
@@ -13,31 +13,33 @@ import { Context } from '@shopify/app-bridge-react';
 
 import * as PropTypes from 'prop-types';
 
-const GET_PRODUCTS_BY_ID = gql`
-  query getProducts($ids: [ID!]!) {
-    nodes(ids: $ids) {
-      ... on Product {
-        title
-        handle
-        descriptionHtml
-        id
-        images(first: 1) {
-          edges {
-            node {
-              originalSrc
-              altText
+const GET_ALL_PRODUCTS = gql`
+  query getProducts {
+    products(first: 10) {
+      edges {
+        node {
+          title
+          handle
+          descriptionHtml
+          id
+          images(first: 1) {
+            edges {
+              node {
+                originalSrc
+                altText
+              }
+            }
+          }
+          variants(first: 1) {
+            edges {
+              node {
+                price
+                id
+              }
             }
           }
         }
-        variants(first: 1) {
-          edges {
-            node {
-              price
-              id
-            }
-          }
-        }
-      }
+     }
     }
   }
 `;
@@ -57,40 +59,41 @@ class ResourceListWithProducts extends React.Component {
 
     const twoWeeksFromNow = new Date(Date.now() + 12096e5).toDateString();
     return (
-      <Query query={GET_PRODUCTS_BY_ID} variables={{ ids: store.get('ids') }}>
+      <Query query={GET_ALL_PRODUCTS}>
         {({ data, loading, error }) => {
           if (loading) { return <div>Loading…</div>; }
           if (error) { return <div>{error.message}</div>; }
           console.log(data);
+          stop();
           return (
             <Card>
               <ResourceList
                 showHeader
                 resourceName={{ singular: 'Product', plural: 'Products' }}
-                items={data.nodes}
+                items={data.products.edges}
                 renderItem={(item) => {
                   const media = (
                     <Thumbnail
                       source={
-                        item.images.edges[0]
-                          ? item.images.edges[0].node.originalSrc
+                        item.node.images.edges[0]
+                          ? item.node.images.edges[0].node.originalSrc
                           : ''
                       }
                       alt={
-                        item.images.edges[0]
-                          ? item.images.edges[0].node.altText
+                        item.node.images.edges[0]
+                          ? item.node.images.edges[0].node.altText
                           : ''
                       }
                     />
                   );
-                  const price = item.variants.edges[0].node.price;
+                  const price = item.node.variants.edges[0].node.price;
                   return (
                     <ResourceList.Item
-                      id={item.id}
+                      id={item.node.id}
                       media={media}
-                      accessibilityLabel={`View details for ${item.title}`}
+                      accessibilityLabel={`View details for ${item.node.title}`}
                       onClick={() => {
-                        store.set('item', item);
+                        store.set('item', item.node);
                         redirectToProduct();
                       }
                       }
@@ -99,7 +102,7 @@ class ResourceListWithProducts extends React.Component {
                         <Stack.Item fill>
                           <h3>
                             <TextStyle variation="strong">
-                              {item.title}
+                              {item.node.title}
                             </TextStyle>
                           </h3>
                         </Stack.Item>
